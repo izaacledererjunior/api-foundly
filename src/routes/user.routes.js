92ca96d3feaaa4bd9b63fa5427d6d8085465302d
Izaac <izaac.lederer@watch.tv.br>
2025-04-24 00:31:10 -0300

    fix get by email

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,8 @@ import userController from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/', userController.listUsers);
-router.get('/:id', userController.getUserById);
 router.get('/email', userController.getUserByEmail);
+router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
